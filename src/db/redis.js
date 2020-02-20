f813460cf5dd6c148e797a8901b40fce76c56157
Ra <rrequero@gmail.com>
2020-02-20 11:03:32 +0100

    remove unnecesary config and not create client if redis is not enabled

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,13 +1,16 @@
 const RedisCache = require('./redis-tag-cache');
 const config = require('../config');
 
-const cache = new RedisCache({
-  redis: {
-    port: config.redis.port,
-    host: config.redis.host,
-    return_buffers: true
-  },
-  defaultTimeout: 86400 // Expire records after a day (even if they weren't invalidated)
-});
+let cache = null;
+if (config.redis.enabled) {
+  cache = new RedisCache({
+    redis: {
+      port: config.redis.port,
+      host: config.redis.host,
+      return_buffers: true,
+    },
+    defaultTimeout: 86400, // Expire records after a day (even if they weren't invalidated)
+  });
+}
 
 module.exports = cache;

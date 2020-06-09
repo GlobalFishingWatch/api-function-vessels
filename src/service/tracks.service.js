2edc581d37e3ea2c82aac99905dd49f9ea874f74
Ra <rrequero@gmail.com>
2020-06-09 15:55:22 +0200

    update date fishing

--- src/service/tracks.service.js
+++ src/service/tracks.service.js
@@ -156,7 +156,7 @@ module.exports = ({ dataset, additionalFeatures = [], params, fields }) => {
         feature => feature.databaseField,
       );
       let startDate = new Date('2019-01-01T00:00:00.000Z');
-      let endDate = new Date('2019-08-31T23:59:59.000Z');
+      let endDate = new Date('2019-12-31T23:59:59.000Z');
       if (params.startDate && params.startDate > startDate) {
         startDate = params.startDate;
       }
